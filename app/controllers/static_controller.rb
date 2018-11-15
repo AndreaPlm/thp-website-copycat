b5cfb8c3c536d7960ac1515661cf585f54d68c2d
TinQk <potie.quentin@gmail.com>
2018-11-15 21:31:27 +0100

    add form fonctionnel pour inscription newsletter par usertype

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -6,9 +6,31 @@ class StaticController < ApplicationController
   def students
   end
 
+  def student_create
+    client = Client.new
+    client.email = params[:email]
+    client.client_type = "student"
+    client.save
+  end
+
   def lyon
+  end
+
+  def lyon_create
+    client = Client.new
+    client.email = params[:email]
+    client.client_type = "town"
+    client.save
   end
 
   def corp
   end
+
+  def corp_create
+    client = Client.new
+    client.email = params[:email]
+    client.client_type = "company"
+    client.save
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,13 @@ Rails.application.routes.draw do
   get '/', to: 'static#index'
 
   get '/lyon', to: 'static#lyon'
+  post '/lyon', to: 'static#lyon_create'
 
   get '/students', to: 'static#students'
+  post '/students', to: 'static#student_create'
 
   get '/corp', to: 'static#corp'
+  post '/corp', to: 'static#corp_create'
+
+
 end
